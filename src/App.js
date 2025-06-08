65a13868ef2af8fd8e17c4a27d0073924d8d3251
Arpit <111arpit1@gmail.com>
2025-06-08 15:14:57 +0530

    minor bug fixed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import React from 'react';
+
 import AllTasks from './components/AllTasks';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-toastify/dist/ReactToastify.css';
-import AddTask from './components/AddTask';
-import EditTask from './components/Edit';
+
 import { BrowserRouter as Router,Routes, Route } from 'react-router-dom';
 import './style.css';
-import Footer from './components/Footer';
+
 
 function App() {
   return (
